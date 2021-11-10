91367c5cfefda034ebc8cb221d89c0ef1e3b9ed9
Caleb E Klaus <90652137+calebklaus317@users.noreply.github.com>
2021-11-10 14:26:04 -0700

    Background images/icon

--- accounts/models.py
+++ accounts/models.py
@@ -4,3 +4,10 @@ from django.db import models
 
 class CustomUser(AbstractUser):
 	zipcode = models.PositiveIntegerField(null=True, blank=True)
+
+"""class Mountain(models.Model):
+	mountain = models.CharField(max_length=50) #add a max_length variable
+	mt_location = models.PositiveIntegerField(null=True, blank=True)#need to figure out how Google API sends location variable
+	winter_season = models.DateField() #need to figure out relationship with mountain
+	summer_season = models.DateField()
+"""
