5845fe97d63afe6117c718ac2f20fb31223869a1
rkemp12 <rk179269.umconnect.umt.edu>
2021-12-06 14:31:57 -0700

    sqlite changes, settingschanges, views, resorts

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     #Local Apps
     'accounts',
     'pages',
+    'scraping.apps.ScrapingConfig',
 ]
 
 MIDDLEWARE = [
@@ -139,3 +140,9 @@ LOGOUT_REDIRECT_URL = 'home'
 #CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+CELERY_BROKER_URL = 'amqp://localhost:15672/#/'
+CELERY_RESULT_BACKEND = "rpc://"
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'MST'
--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,26 @@
 from django.views.generic import TemplateView
-
+from scraping.models import MountainDates
 
 class HomePageView(TemplateView):
 	template_name = 'home.html'
 
 class ResortsPageView(TemplateView):
+		
+	def get_context_data(self, **kwargs):
+		
+		context = super().get_context_data(**kwargs)
+		data = MountainDates.objects.filter(mountain='snowbowl')
+		context['snowbowlDate'] = data
+
+		
+		if MountainDates.mountain == 'snowbowl':
+			context['date'] = MountainDates.winter_season
+		elif MountainDates.mountain == 'bigsky':
+			context['date'] = MountainDates.winter_season
+		
+		return context	
+	
+	
 	template_name = 'resorts_view.html'
 
 class LocationPageView(TemplateView):
